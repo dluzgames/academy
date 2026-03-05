a17f5cbc1bc8c00e1e89325c1d5c173e5f1ae53b
DluzGG <dluzgg@example.com>
2026-03-04 23:14:24 -0300

    fix: adjust capacitor config server scheme and hostname to handle firebase auth redirect from within the apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dluz.elitevelocity',
   appName: 'Elite Velocity',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    androidScheme: 'https',
+    hostname: 'elite-velocity-v2-dluz.web.app',
+    allowNavigation: [
+      "elite-velocity-v2-dluz.firebaseapp.com",
+      "*.firebaseapp.com"
+    ]
+  }
 };
 
 export default config;
